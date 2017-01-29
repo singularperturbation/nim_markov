378d88e126f387965100e266a37e62dce5d67f71
singularperturbation <sloanes.k@gmail.com>
2017-01-29 13:42:36 -0600

    Make preprocess script better

--- preprocess.sh
+++ preprocess.sh
@@ -4,15 +4,16 @@
 
 jq -r '.[] | select(.type == "message") | select(has("subtype")==false) | .text | gsub("\n"; " ") | gsub("&gt;";" ") | gsub("&lt;";"") | gsub("&amp;";" ")' \
   | sed -e 's/<.*>/ /g' \
-  | sed -e 's/`/ /g' \
+  | sed -e 's/`.*`/ /g' \
   | sed -E 's/[[:space:]]+/ /g' \
   | ruby -pe '$_ = if $_ =~ /[[:alpha:]]/ then $_ else "" end' \
   | sed -e 's/[“”]/"/g' \
+  | sed -e 's/‘//g' \
   | grep -v '^$'
 
 # Remove URLs
-# Remove inline backticks
+# Remove inline backticks and what's inside them
 # Remove extraneous whitespace (won't be necessary when tokenizing)
 # Has to have at least one alphanumeric character
-# Convert 'weird' quotes to normal quotes
+# Convert 'weird' quotes to normal quotes (and remove weird single quotes)
 # Remove empty lines
