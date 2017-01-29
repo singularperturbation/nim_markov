ffbbb1d1875ae37bec666af82790d70b0fa8fb1e
singularperturbation <sloanes.k@gmail.com>
2017-01-29 12:54:00 -0600

    Don't do lower-case normalization when preprocessing

--- preprocess.sh
+++ preprocess.sh
@@ -3,10 +3,16 @@
 # Reads JSON from Slack dump in stdin, normalized messages go to stdout
 
 jq -r '.[] | select(.type == "message") | select(has("subtype")==false) | .text | gsub("\n"; " ") | gsub("&gt;";" ") | gsub("&lt;";"") | gsub("&amp;";" ")' \
-  | sed -e 's/<.*>/ /g' \                                         # Remove URLs
-  | sed -e 's/`/ /g' \                                            # Remove inline quotes
-  | sed -E 's/[[:space:]]+/ /g' \                                 # Remove extraneous whitespace (won't be necessary when tokenizing)
-  | ruby -pe '$_ = $_.lstrip.downcase' \
-  | ruby -pe '$_ = if $_ =~ /[[:alpha:]]/ then $_ else "" end' \  # Has to have at least one alphanumeric character
-  | sed -e 's/[“""]//g' \                                         # Remove weird quotes
-  | grep -v '^$'                                                  # Remove empty lines
+  | sed -e 's/<.*>/ /g' \
+  | sed -e 's/`/ /g' \
+  | sed -E 's/[[:space:]]+/ /g' \
+  | ruby -pe '$_ = if $_ =~ /[[:alpha:]]/ then $_ else "" end' \
+  | sed -e 's/[“”]/"/g' \
+  | grep -v '^$'
+
+# Remove URLs
+# Remove inline backticks
+# Remove extraneous whitespace (won't be necessary when tokenizing)
+# Has to have at least one alphanumeric character
+# Convert 'weird' quotes to normal quotes
+# Remove empty lines
