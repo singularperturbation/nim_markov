b6c8587f98733b574d162ed54d9d86b253b3fda8
singularperturbation <sloanes.k@gmail.com>
2017-02-03 21:28:56 -0600

    Improve cleaning script
    - Don't filter out numeric
    - Normalize quotes
    - Don't have ``` sections

--- preprocess.sh
+++ preprocess.sh
@@ -2,18 +2,21 @@
 
 # Reads JSON from Slack dump in stdin, normalized messages go to stdout
 
-jq -r '.[] | select(.type == "message") | select(has("subtype")==false) | .text | gsub("\n"; " ") | gsub("&gt;";" ") | gsub("&lt;";"") | gsub("&amp;";" ")' \
+jq -r '.[] | select(.type == "message") | select(has("subtype")==false) | .text | select(contains("```")==false) | gsub("&gt;";" ") | gsub("&lt;";"") | gsub("&amp;";" ")' \
   | sed -e 's/<.*>/ /g' \
   | sed -e 's/`.*`/ /g' \
+  | sed -e 's/`/ /g' \
   | sed -E 's/[[:space:]]+/ /g' \
-  | ruby -pe '$_ = if $_ =~ /[[:alpha:]]/ then $_ else "" end' \
+  | ruby -pe '$_ = if $_ =~ /[[:alnum:]]/ then $_ else "" end' \
   | sed -e 's/[“”]/"/g' \
-  | sed -e 's/‘//g' \
+  | sed -e "s/‘/'/g" \
   | grep -v '^$'
 
 # Remove URLs
 # Remove inline backticks and what's inside them
 # Remove extraneous whitespace (won't be necessary when tokenizing)
 # Has to have at least one alphanumeric character
-# Convert 'weird' quotes to normal quotes (and remove weird single quotes)
+# Convert 'weird' quotes to normal quotes (and same with weird single quotes)
 # Remove empty lines
+
+# Filtering out 
